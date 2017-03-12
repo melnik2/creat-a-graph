297a0183f881c2af92b4e1de53c46794fad2c108
melnik2 <jeywen@yandex.ru>
2017-03-12 16:51:14 +0300

    Add files via upload

---
+++ .gitignore/Unit48.cpp
@@ -0,0 +1,309 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <fstream>       // read-save file
+#include <math.h>       // ceil
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include<algorithm>
+
+#include "Unit48.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "sDialogs"
+#pragma resource "*.dfm"
+TForm1 *Form1;
+
+using namespace std;
+
+FILE *fp;
+
+int rw1; // rows
+int clm1; // column
+int sum1; // sum
+int x=0;
+int y=0;
+char cd1;
+int cnt1=0;
+int cnt2=0;
+int cnt3=0;
+int cnt4=0;
+int grn1;
+int grn2;
+int itr1;
+int n;
+int max1;
+int max2;
+
+int *iMark1 = new int [n];
+char *str1 = new char[4];
+char *str2 = new char[4];
+char *str3 = new char[4];
+char *s3 = new char[8];
+char *cx1=new char[4];
+int *iMark2;
+
+int func1();
+
+AnsiString FileName1;
+AnsiString FileName2;
+
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+//fhftfh
+}
+
+void __fastcall TForm1::TrackMouseUp(TObject *Sender, TMouseButton Button, TShiftState Shift, int X, int Y)
+{
+max1=0;
+max2=TrackBar3->Position;
+
+for (int i=2*(grn1-1); i<=2*(grn2-1); i+=2){ //cnt1+cnt2,rw1
+y=iMark2[i+1];
+max1=max(abs(y),max1);
+}
+
+TrackBar3->Min=max1/10;
+TrackBar3->Max=10*max1;
+TrackBar3->Position=max2;
+
+if(CheckBox1->Checked == true)Memo1->Clear();
+Series1->Clear();
+Series2->Clear();
+Series3->Clear();
+Series4->Clear();
+
+for (int i=2*(grn1-1); i<=2*(grn2-1); i+=2){ //cnt1+cnt2,rw1
+x=iMark2[i];
+y=iMark2[i+1];
+itoa(x, str1, 10);
+itoa(y, str3, 10);
+strcpy(s3, str1);
+strcat(s3, str2);
+strcat(s3, str3);
+if(CheckBox1->Checked == true)Memo1->Lines->Add(s3);
+Series1->Add(y,x,clBlue);
+Series2->Add(0,x,clGreen);
+ }
+
+Series3->AddXY(0,max2,0,clSilver);
+Series4->AddXY(0,-max2,0,clSilver);
+
+for (int i=0; i<4; i++){
+str1[i]=0;
+str3[i]=0;
+ }
+for (int i=0; i<8; i++){
+s3[i]=0;
+ }
+//grn1=grn2=0;
+//delete [] iMark2;
+}
+
+void __fastcall TForm1::TrackBar1Change(TObject *Sender)
+{
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+
+Label5->Caption=TrackBar1->Position;
+Label9->Caption= IntToStr(grn1);
+Label10->Caption= IntToStr(grn2);
+
+((THackTrackBar*)TrackBar1)->OnMouseUp = TrackMouseUp;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::TrackBar2Change(TObject *Sender)
+{
+itr1=TrackBar2->Position;
+
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+
+Label9->Caption= IntToStr(grn1);
+Label10->Caption= IntToStr(grn2);
+
+ TrackBar1->Min=itr1;
+ TrackBar1->Max=rw1-itr1;
+
+ Label23->Caption=TrackBar2->Position;
+ Label6->Caption=IntToStr(2*itr1)+"/"+IntToStr(itr1);
+
+ Label5->Caption=TrackBar1->Position;
+ Label11->Caption=TrackBar1->Min;
+ Label12->Caption=TrackBar1->Max;
+ ((THackTrackBar*)TrackBar2)->OnMouseUp = TrackMouseUp;
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::TrackBar3Change(TObject *Sender)
+{
+ ((THackTrackBar*)TrackBar3)->OnMouseUp = TrackMouseUp;
+}
+//---------------------------------------------------------------------------
+
+int func1(){
+ifstream file1 (FileName2.c_str()); //  "C:\\Users\\Freag2\\Documents\\si\\Project48.Grafic.Po_data_file_txt\\B15.txt", ios_base::in | ios_base::out
+while((cd1 = file1.get())!= EOF){
+switch (cd1) {
+case '\t':  //getline() сканирует на предмет разделителя
+cnt1++;
+break;
+case '\n':
+cnt2++;
+break;
+default:
+break;
+ }
+}
+file1.clear();
+file1.close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Open1Click(TObject *Sender)
+{
+//SelectDirectory(FileName1,TSelectDirOpts() << sdAllowCreate << sdPerformCreate << sdPrompt, 0);
+
+SelectDirectory("","",FileName1);
+sOpenDialog1->Execute();
+sOpenDialog1->InitialDir = FileName1;
+//ShowMessage(sOpenDialog1->FileName);
+FileName2=sOpenDialog1->FileName;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+
+//fp=fopen(FileName1.c_str(),"r");
+ifstream file3 (FileName2.c_str()); // "C:\\Users\\Freag2\\Documents\\si\\Project48.Grafic.Po_data_file_txt\\B15.txt"
+if((file3)!=NULL)   Label1->Caption="File - OK";
+else Label1->Caption="File - BAD";
+file3.clear();
+file3.close();
+
+
+func1();
+
+rw1=cnt2;
+clm1=(cnt1+cnt2)/cnt2;
+sum1=cnt1+cnt2;
+
+Label2->Caption="Rows "+IntToStr(rw1);
+Label3->Caption="Column "+IntToStr(clm1);
+Label4->Caption="Sum "+IntToStr(sum1);
+
+
+itr1= 0.01*rw1;
+Label6->Caption=IntToStr(2*itr1)+"/"+IntToStr(itr1);
+TrackBar1->Min=itr1;
+TrackBar1->Max=rw1-itr1;
+TrackBar1->Position=10*itr1;
+TrackBar2->Position=0.5*rw1-1;
+Label5->Caption=IntToStr(10*itr1);
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+TrackBar2->Min=0.01*rw1;
+TrackBar2->Max=0.5*rw1;
+
+Label10->Caption=IntToStr(rw1);
+Label11->Caption=TrackBar1->Min;
+Label12->Caption=TrackBar1->Max;
+Label21->Caption=TrackBar2->Min;
+Label22->Caption=TrackBar2->Max;
+Label23->Caption=0.1*rw1;
+
+delete [] iMark1;
+int *iMark1 = new int [cnt1+cnt2+1];
+
+ifstream file2 (FileName2.c_str());  //  "C:\\Users\\Freag2\\Documents\\si\\Project48.Grafic.Po_data_file_txt\\B15.txt"
+while((cd1 = file2.get())!= EOF){
+switch (cd1) {
+case '\t':
+iMark1[cnt4] = atoi (cx1);
+cnt3=0;
+cnt4++;
+
+for (int i=0; i<4; i++){
+cx1[i]=0;
+}
+
+break;
+case '\n':
+iMark1[cnt4] = atoi (cx1);
+cnt3=0;
+cnt4++;
+
+for (int i=0; i<4; i++){
+cx1[i]=0;
+}
+
+break;
+default:
+cx1[cnt3]= cd1;  //case 1: value true 1 false 11
+cnt3++;
+break;
+ }
+}
+file2.clear();
+file2.close();
+
+iMark2=iMark1;
+
+Memo1->Clear();
+str2="	";
+
+for (int i=0; i<cnt1+cnt2; i+=2){
+y=iMark1[i+1];
+max1=max(abs(y),max1);
+}
+
+TrackBar3->Min=max1/10;
+TrackBar3->Max=10*max1;
+TrackBar3->Position=max1;
+
+for (int i=0; i<cnt1+cnt2; i+=2){ //cnt1+cnt2,rw1
+x=iMark1[i];
+y=iMark1[i+1];
+itoa(x, str1, 10);
+itoa(y, str3, 10);
+strcpy(s3, str1);
+strcat(s3, str2);
+strcat(s3, str3);
+Series1->Add(y,x,clBlue);
+Series2->Add(0,x,clGreen);
+ }
+
+Series3->AddXY(0,max1,0,clSilver);
+Series4->AddXY(0,-max1,0,clSilver);
+
+for (int i=0; i<4; i++){
+str1[i]=0;
+str3[i]=0;
+ }
+for (int i=0; i<8; i++){
+s3[i]=0;
+ }
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Exit1Click(TObject *Sender)
+{
+ //mExcel.OleProcedure("Quit");
+ Form1->Close();
+}
+//---------------------------------------------------------------------------
+
---
+++ .gitignore/Unit51.cpp
@@ -0,0 +1,437 @@
+
+#include <vcl.h>
+#pragma hdrstop
+#include <fstream>       // read-save file
+//#include <math.h>       // ceil
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <cstdlib>
+//#include <cstring>
+//#include <ComObj.hpp>
+#include <utilcls.h>
+#include<algorithm>
+
+#include "Unit51.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "sDialogs"
+#pragma resource "*.dfm"
+TForm1 *Form1;
+
+using namespace std;
+
+FILE *file1;
+
+Variant mExcel;
+String strA1, stred1, stred2;
+
+int arr1n[9];
+
+int in1,in2;
+int x,y;
+
+char ca1, ca2;
+
+char *str1 = new char[4];
+char *str2 = "	";
+char *str3 = new char[4];
+char *s3 = new char[8];
+
+int *iMarkX;
+int *iMarkY;
+char *iMarkW;
+
+int min1;
+int idn1,idn2;
+
+void func1(int i);
+int* func2(char a1, char a2);
+
+int grn1;
+int grn2;
+int itr1;
+int n;
+int max1;
+int max2;
+
+int *iMark1;
+int *iMark2;
+
+AnsiString FileName1;
+AnsiString FileName2;
+
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+arr1n[0]=arr1n[1]=arr1n[2]=arr1n[3]=arr1n[4]=arr1n[5]=arr1n[6]=arr1n[7]=arr1n[8]=1;
+
+for (int i=0; i<8; i++){
+func1(i);
+ComboBox1->Items->Add(stred1), ComboBox2->Items->Add(stred1);
+}
+ /*
+ for (int i=0; i<8; i++){
+ func1(i);
+ while ((strA1 =mExcel.OlePropertyGet("Cells", arr1n[i], stred1.c_str()).OlePropertyGet("Value")).Length()!=0) arr1n[i]++;
+ --arr1n[i];
+ }
+
+ for (int i=0; i<9; i++){
+ func1(i);
+ ((TLabel*)Form1->Components[i])->Caption=stred1+" "+IntToStr(arr1n[i]);
+ }
+ */
+
+ Memo1->Clear();
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+
+  if(CheckBox1->Checked == true) Memo1->Clear();
+  Series1->Clear();
+  Series2->Clear();
+  Series3->Clear();
+  Series4->Clear();
+
+  stred2=ComboBox1->Text;
+  iMarkW=stred2.c_str();
+  ca1=iMarkW[0];
+  stred2=ComboBox2->Text;
+  iMarkW=stred2.c_str();
+  ca2=iMarkW[0];
+  delete [] iMarkW;
+
+  delete [] iMarkX;
+  delete [] iMarkY;
+
+  func2(ca1, ca2); //A B - idn
+
+ mExcel = CreateOleObject("Excel.Application");
+ //AnsiString FileName=GetCurrentDir()+"\\data5.xlsx";
+ mExcel.OlePropertyGet("WorkBooks").OleProcedure("Open", FileName2.c_str());
+ Label1->Caption="Excel open";
+ ProgressBar1->Position=1;
+ while ((strA1 =mExcel.OlePropertyGet("Cells", arr1n[idn1-1], idn1).OlePropertyGet("Value")).Length()!=0) arr1n[idn1-1]++;
+ Label1->Caption="in1 received";
+ ProgressBar1->Position=2;
+ while ((strA1 =mExcel.OlePropertyGet("Cells", arr1n[idn2-1], idn2).OlePropertyGet("Value")).Length()!=0) arr1n[idn2-1]++;
+ Label1->Caption="int2 received";
+ ProgressBar1->Position=3;
+
+ in1 = arr1n[idn1-1]-1;
+ in2 = arr1n[idn2-1]-1;
+ arr1n[idn1-1]=arr1n[idn2-1]=1;
+
+ min1=in1;
+ min1=min(min1,in2);
+
+ int *iMarkX = new int [min1];
+ int *iMarkY = new int [min1];
+ Label1->Caption="XY create";
+ ProgressBar1->Position=4;
+
+ for (int i=0; i<min1; i++){
+ iMarkX[i] = mExcel.OlePropertyGet("Cells", i+1, idn1).OlePropertyGet("Value");
+ }
+ Label1->Caption="X received";
+ ProgressBar1->Position=5;
+ for (int i=0; i<min1; i++){
+ iMarkY[i] = mExcel.OlePropertyGet("Cells", i+1, idn2).OlePropertyGet("Value");
+ }
+ Label1->Caption="Y received";
+ ProgressBar1->Position=6;
+
+ mExcel.OleProcedure("Quit");
+ Label1->Caption="Excel closed";
+ ProgressBar1->Position=0;
+
+  for(int i=0;i<min1;i++){
+  x=iMarkX[i];
+  y=iMarkY[i];
+  if(CheckBox1->Checked == true){
+  itoa(x, str1, 10);
+  itoa(y, str3, 10);
+  strcpy(s3, str1);
+  strcat(s3, str2);
+  strcat(s3, str3);
+  Memo1->Lines->Add(s3);
+  }
+  Series1->Add(y,x,clBlue);
+  Series2->Add(0,x,clGreen);
+  }
+ Label1->Caption="inform";
+
+  for (int i=0; i<4; i++){
+  str1[i]=0;
+  str3[i]=0;
+  }
+  for (int i=0; i<8; i++){
+  s3[i]=0;
+  }
+
+for (int i=0; i<min1; i++){
+y=iMarkY[i];
+max1=max(abs(y),max1);
+}
+
+TrackBar3->Min=max1/10;
+TrackBar3->Max=10*max1;
+TrackBar3->Position=max1;
+
+Series3->AddXY(0,max1,0,clSilver);
+Series4->AddXY(0,-max1,0,clSilver);
+
+itr1= 0.01*min1;
+Label6->Caption="range "+IntToStr(2*itr1)+"/"+IntToStr(itr1);
+TrackBar1->Min=itr1;
+TrackBar1->Max=min1-itr1;
+TrackBar1->Position=10*itr1;
+TrackBar2->Position=0.5*min1-1;
+Label5->Caption=IntToStr(10*itr1);
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+TrackBar2->Min=0.01*min1;
+TrackBar2->Max=0.5*min1;
+
+Label10->Caption=IntToStr(min1);
+Label11->Caption=TrackBar1->Min;
+Label12->Caption=TrackBar1->Max;
+Label21->Caption=TrackBar2->Min;
+Label22->Caption=TrackBar2->Max;
+Label23->Caption=0.1*min1;
+
+delete [] iMark1;
+delete [] iMark2;
+
+iMark1=iMarkX;
+iMark2=iMarkY;
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TrackMouseUp(TObject *Sender, TMouseButton Button, TShiftState Shift, int X, int Y)
+{
+
+max1=0;
+max2=TrackBar3->Position;
+
+for (int i=grn1-1; i<grn2; i++){
+y=iMark2[i];
+max1=max(abs(y),max1);
+}
+
+TrackBar3->Min=max1/10;
+TrackBar3->Max=10*max1;
+TrackBar3->Position=max2;
+
+if(CheckBox1->Checked == true)Memo1->Clear();
+Series1->Clear();
+Series2->Clear();
+Series3->Clear();
+Series4->Clear();
+
+  int size1=0;
+  for (int i=grn1-1; i<grn2; i++){
+  x=iMark1[i];
+  y=iMark2[i];
+  if(CheckBox1->Checked == true){
+  itoa(x, str1, 10);
+  itoa(y, str3, 10);
+  strcpy(s3, str1);
+  strcat(s3, str2);
+  strcat(s3, str3);
+  Memo1->Lines->Add(s3);
+  switch (size1){
+  case 0:
+  if (i>0.1*grn2) ProgressBar1->Position=1, size1=1;
+  break;
+  case 1:
+  if (i>0.3*grn2) ProgressBar1->Position=2, size1=2;
+  break;
+  case 2:
+  if (i>0.5*grn2) ProgressBar1->Position=3, size1=3;
+  break;
+  case 3:
+  if (i>0.7*grn2) ProgressBar1->Position=4, size1=4;
+  break;
+  case 4:
+  if (i>0.9*grn2) ProgressBar1->Position=0, size1=5;
+  break;
+    }
+  }
+  Series1->Add(y,x,clBlue);
+  Series2->Add(0,x,clGreen);
+  }
+
+  for (int i=0; i<4; i++){
+  str1[i]=0;
+  str3[i]=0;
+  }
+  for (int i=0; i<8; i++){
+  s3[i]=0;
+  }
+  
+  Series3->AddXY(0,max2,0,clSilver);
+  Series4->AddXY(0,-max2,0,clSilver);
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TrackBar1Change(TObject *Sender)
+{
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+
+Label5->Caption=TrackBar1->Position;
+Label9->Caption= IntToStr(grn1);
+Label10->Caption= IntToStr(grn2);
+
+((THackTrackBar*)TrackBar1)->OnMouseUp = TrackMouseUp;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TrackBar2Change(TObject *Sender)
+{
+itr1=TrackBar2->Position;
+
+grn1=TrackBar1->Position-itr1+1;
+grn2=TrackBar1->Position+itr1;
+
+Label9->Caption= IntToStr(grn1);
+Label10->Caption= IntToStr(grn2);
+
+ TrackBar1->Min=itr1;
+ TrackBar1->Max=min1-itr1;
+
+ Label23->Caption=TrackBar2->Position;
+ Label6->Caption=IntToStr(2*itr1)+"/"+IntToStr(itr1);
+
+ Label5->Caption=TrackBar1->Position;
+ Label11->Caption=TrackBar1->Min;
+ Label12->Caption=TrackBar1->Max;
+ ((THackTrackBar*)TrackBar2)->OnMouseUp = TrackMouseUp;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::TrackBar3Change(TObject *Sender)
+{
+((THackTrackBar*)TrackBar3)->OnMouseUp = TrackMouseUp;
+}
+//---------------------------------------------------------------------------
+int* func2(char a1, char a2){
+
+  switch (a1){
+  case 'A':
+  idn1=1;
+  break;
+  case 'B':
+  idn1=2;
+  break;
+  case 'C':
+  idn1=3;
+  break;
+  case 'D':
+  idn1=4;
+  break;
+  case 'E':
+  idn1=5;
+  break;
+  case 'F':
+  idn1=6;
+  break;
+  case 'G':
+  idn1=7;
+  break;
+  case 'H':
+  idn1=8;
+  break;
+  }
+  
+  switch (a2){
+  case 'A':
+  idn2=1;
+  break;
+  case 'B':
+  idn2=2;
+  break;
+  case 'C':
+  idn2=3;
+  break;
+  case 'D':
+  idn2=4;
+  break;
+  case 'E':
+  idn2=5;
+  break;
+  case 'F':
+  idn2=6;
+  break;
+  case 'G':
+  idn2=7;
+  break;
+  case 'H':
+  idn2=8;
+  break;
+  }
+
+  //return im1;
+}
+//---------------------------------------------------------------------------
+
+void func1(int i){
+  switch (i){
+  case 0:
+  stred1="A";
+  break;
+  case 1:
+  stred1="B";
+  break;
+  case 2:
+  stred1="C";
+  break;
+  case 3:
+  stred1="D";
+  break;
+  case 4:
+  stred1="E";
+  break;
+  case 5:
+  stred1="F";
+  break;
+  case 6:
+  stred1="G";
+  break;
+  case 7:
+  stred1="H";
+  break;
+default:
+break;
+  }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Open1Click(TObject *Sender)
+{
+ //SelectDirectory(FileName1,TSelectDirOpts() << sdAllowCreate << sdPerformCreate << sdPrompt, 0);
+
+SelectDirectory("","",FileName1);
+sOpenDialog1->Execute();
+sOpenDialog1->InitialDir = FileName1;
+//ShowMessage(sOpenDialog1->FileName);
+FileName2=sOpenDialog1->FileName;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::E1Click(TObject *Sender)
+{
+AnsiString excl1 = (AnsiString)mExcel;
+//empty() length()
+if (excl1!="") mExcel.OleProcedure("Quit");
+ Form1->Close();
+}
+//---------------------------------------------------------------------------
+
